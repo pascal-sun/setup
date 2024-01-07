6ca0af3b45b6cc78263c5673e821ae6974d1ec6a
Pascal Sun <35567930+pascal-sun@users.noreply.github.com>
2024-01-07 17:55:37 +0100

    fix: remove -e flag

--- pre-install.sh
+++ pre-install.sh
@@ -3,12 +3,12 @@
 GREEN="\033[32m"
 ENDCOLOR="\033[0m"
 
-echo -e "${GREEN}[*] Upgrading...${ENDCOLOR}"
+echo "${GREEN}[*] Upgrading...${ENDCOLOR}"
 sudo apt-get update && sudo apt-get upgrade -y
 
-echo -e "${GREEN}[*] Installing pipx...${ENDCOLOR}"
+echo "${GREEN}[*] Installing pipx...${ENDCOLOR}"
 sudo apt-get install -y pipx
 pipx ensurepath
 
-echo -e "${GREEN}[*] Installing Ansible...${ENDCOLOR}"
+echo "${GREEN}[*] Installing Ansible...${ENDCOLOR}"
 pipx install --include-deps ansible
