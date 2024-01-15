f59d43ac59c75e5c7cebc25d3b3fb3dc90d8dcb9
Pascal SUN <pascal.sun@patrowl.io>
2024-01-15 21:18:56 +0100

    test

--- pre-install.sh
+++ pre-install.sh
@@ -4,10 +4,10 @@ GREEN="\033[32m"
 ENDCOLOR="\033[0m"
 
 echo "${GREEN}[*] Upgrading...${ENDCOLOR}"
-sudo apt-get update
+apt-get update
 
 echo "${GREEN}[*] Installing pipx...${ENDCOLOR}"
-sudo apt-get install -y pipx
+apt-get install -y pipx
 pipx ensurepath
 
 echo "${GREEN}[*] Installing Ansible...${ENDCOLOR}"
